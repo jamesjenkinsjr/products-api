a2041cbc11000bf0a0d56e6e9a453dc8818c13ff
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-14 18:33:56 -0400

    Create products router, wire to top-level app

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const serverApp = express();
 
 const PORT = process.env.port || 5000; //necessary for Heroku deployment
 
+//routers
+const productRouter = require('./routers/products');
+
+serverApp.use(productRouter); //register the router with the application
+
 serverApp.get('/', (req, res) => {
     res.send('HELLO! I work');
 })

e6d9f32a63bd64d58c4e0130c5059d97202eae66
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-14 19:29:57 -0400

    Create mock product data to serve to as get request response

---
+++ mocks/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        _id: 'dfdgfhd',
+        name: 'Javelin (CAUTION)',
+        price: 500,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: 'yfiokjvh',
+        name: 'Snail',
+        price: 3.99,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: 'fchgkvjhb',
+        name: 'Rug',
+        price: 96.75,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    }
+];
+
+
+
+module.exports = products;

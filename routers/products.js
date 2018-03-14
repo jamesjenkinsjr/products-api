4991794750f4256f8a34030d8ffe8bd9456292a3
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-14 19:47:47 -0400

    Create helper to transform data to object of products

--- routers/products.js
+++ routers/products.js
@@ -2,9 +2,27 @@ const express = require('express');
 const router = express.Router();
 const mockProducts = require('../mocks/products');
 
+const productArrToObj = (arrayOfProducts) => {
+    //create an accumulator object
+    const accumulator = {};
+    //for each product in arrayOfProducts
+    arrayOfProducts.forEach(product => {
+        //grab the id
+        const id = product._id;
+        //copy the products
+        const copy = {...product};
+        //delete _id from interal to product object
+        delete copy._id;
+        //set id value in accumulator object equal to product
+        accumulator[id] = copy;
+    });       
+    //return accumulator
+    return accumulator;
+}
+
 router.get('/products', (req, res) => {
     res.status(200).json({
-        products: mockProducts
+        products: productArrToObj(mockProducts)
     })
 });
 

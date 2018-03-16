b2dc86267b62948a6c7279395818b48e0ffb6dbf
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-15 21:04:29 -0400

    Create get for products by id

--- routers/products.js
+++ routers/products.js
@@ -1,70 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mockProducts = require('../mocks/products');
+const mockProducts = require("../mocks/products");
 
-const Product = require('../models/product');
+const Product = require("../models/product");
 
-const productArrToObj = (arrayOfProducts) => {
-    //create an accumulator object
-    const accumulator = {};
-    //for each product in arrayOfProducts
-    arrayOfProducts.forEach(product => {
-        //grab the id
-        const id = product._id;
-        //copy the products
-        const copy = {...product._doc};
-        //delete _id from interal to product object
-        delete copy._id;
-        //set id value in accumulator object equal to product
-        accumulator[id] = copy;
-    });       
-    //return accumulator
-    return accumulator;
-}
+const productArrToObj = arrayOfProducts => {
+  //create an accumulator object
+  const accumulator = {};
+  //for each product in arrayOfProducts
+  arrayOfProducts.forEach(product => {
+    //grab the id
+    const id = product._id;
+    //copy the products
+    const copy = { ...product._doc };
+    //delete _id from interal to product object
+    delete copy._id;
+    //set id value in accumulator object equal to product
+    accumulator[id] = copy;
+  });
+  //return accumulator
+  return accumulator;
+};
 
-router.get('/products', (req, res) => {
-    Product.find()
-        .exec()
-        .then(allProducts => {
-            res.status(200).json({
-                products: productArrToObj(allProducts)
-            })
-        })
-        .catch(err => {
-            res.status(500).json({
-                msg: 'Woes bro -- shiz iz broke'
-            })
-        });
+router.get("/products", (req, res) => {
+  Product.find()
+    .exec()
+    .then(allProducts => {
+      res.status(200).json({
+        products: productArrToObj(allProducts)
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        msg: "Woes bro -- shiz iz broke"
+      });
+    });
 });
 
-router.get('/products/:id', (req, res) => {
-    const { id } = req.params;
-    const productsObject = productArrToObj(mockProducts); //this will get deleted
-    const selectedProduct = productsObject[id];
-    res.status(200).json({
+router.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  Product.findById(id)
+    .exec()
+    .then(selectedProduct => {
+        const selectedID = selectedProduct._id;
+        const copy = {...selectedProduct._doc};
+        res.status(200).json({
         products: {
-            [id]: selectedProduct
+          [selectedProduct._id]: selectedProduct
         }
-    })
-}); 
 
-router.post('/products', (req, res) => {
-    const product = new Product({
-        name: 'something new',
-        price: 1000,
-        imgSrc: 'https://via.placeholder.com/250x250'
-    });
-    const x = product.save()
-        .then(response => {
-            res.status(200).json({
-                msg: 'successfully created product'
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                msg: 'Your stuff done broek.'
-            });
+      })
+    })
+      .catch(err => {
+        res.status(500).json({
+          msg: "Woes bro -- shiz iz broke -- getByID"
         });
+      });
+  });
+
+router.post("/products", (req, res) => {
+  const product = new Product({
+    name: "something new",
+    price: 1000,
+    imgSrc: "https://via.placeholder.com/250x250"
+  });
+  const x = product
+    .save()
+    .then(response => {
+      res.status(200).json({
+        msg: "successfully created product"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        msg: "Your stuff done broek."
+      });
+    });
 });
 
 module.exports = router; //like export default

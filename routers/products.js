acac779a145b8b0a8326567822058543e834580b
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-19 18:41:28 -0400

    Basic update functionality for PUT router

--- routers/products.js
+++ routers/products.js
@@ -80,7 +80,21 @@ router.post("/products", (req, res) => {
 
 //update (PUT)
 router.put('/products/:id', (req, res) => {
-    res.send('Updating now...');
+    const { id } = req.params;
+    const update = {
+        name: "updated name"
+    };
+    Product.findByIdAndUpdate(id, update)
+        .then(response => {
+            res.status(200).json({
+                msg: "Plopped the pieces of info into the part sought"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: "Please put proper care into parts of your code"
+            });
+        });
 });
 //delete (DELETE)
 router.delete('/products/:id', (req, res) => {
